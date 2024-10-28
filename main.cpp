de4662a350db582bd86b33d2bb213758f04bd37e
j.yusupov <j.yusupov@newuu.uz>
2024-10-28 12:36:50 +0500

    UPD

--- main.cpp
+++ main.cpp
@@ -5,9 +5,25 @@ template <typename T> T mx(T a, T b, T c) {
     return max(max(a, b), c);
 }
 
+template <typename T>
+void swp(T& a, T& b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+
+int ott(int n){
+    if (n==0){  cout << "0" << endl; return 0; }
+    cout << n << endl;
+    return ott(n-1);
+}
+
 int main(){
-    double a, b, c;
+    ott(10);
+    char a, b, c;
     cin >> a >> b >> c;
     cout << mx(a, b, c) << endl;
+    swp(a, b);
+    cout << a << b << endl;
     return 0;
 }
